fed79e08fe90550ffc271eab1818f854b99f83ff
Sh3ldoris <predajna976631010>
2020-10-20 21:57:27 +0200

    Add page template

---
+++ src/app/model/menu.ts
@@ -0,0 +1,5 @@
+export class Menu {
+  public date: Date = new Date();
+  public soups: string[] = ['Hrachovy krem so smotanou', 'Hubovy vyvar s cestovinou'];
+  public main: string[] = ['Pečené morčacie spodné stehno, hubová omáčka s tymiánom a smotanou, dusená ryža, hranolky', 'Pečené bravčové stehno, šťúchané zemiaky s baby špenátom, omáčka'];
+}
