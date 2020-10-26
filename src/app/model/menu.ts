ad42c34f5dd653d7665ca21025294bb33de210aa
lanya <adam.lany@posam.sk>
2020-10-26 20:38:36 +0100

    Language correction, tester was unsatisfied

--- src/app/model/menu.ts
+++ src/app/model/menu.ts
@@ -1,5 +1,5 @@
 export class Menu {
   public date: Date = new Date();
   public soups: string[] = ['Hrachový krém so smotanou', 'Špenátová s vajcom a rozmarínom'];
-  public main: string[] = ['Kuracie prsia na korení, Nivová omáčka so smotanou, dusená ryža, vyprážané zemiakové hranolky', 'Pomaly pečená bravčová krkovička v celku s rascou, pučené zemiaky so slaninkou a cibuľkou, srbská omáčka s paprikou a paradajkou so slaninkou, rukola', 'Azíjské mierne pikantne zeleninové ragú so sójou, pečená dusená tarhoňa, listový šalát'];
+  public main: string[] = ['Kuracie prsia na korení, Nivová omáčka so smotanou, dusená ryža, vyprážané zemiakové hranolky', 'Pomaly pečená bravčová krkovička v celku s rascou, pučené zemiaky so slaninkou a cibuľkou, srbská omáčka s paprikou a paradajkou so slaninkou, rukola', 'Azíjské mierne pikantné klokanie rezy so sójou, pečená dusená tarhoňa, listový šalát'];
 }
